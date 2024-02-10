49d5e5524c4f4c067c7aedcfa3ba9001ae4716ee
JL10 <jorge.lizcano410@gmail.com>
2024-02-09 19:04:36 -0500

    hw with nanoid error

---
+++ contacts.js
@@ -0,0 +1,78 @@
+const fs = require("fs").promises;
+const path = require("path");
+// import { nanoid } from "nanoid";
+
+const contactPath = path.resolve("db/contacts.json");
+
+/*
+ * Comenta y anota el valor
+ * const contactsPath = ;
+ */
+
+// TODO: documenta cada función
+function listContacts() {
+  // ...tu código
+  fs.readFile(contactPath)
+    .then((data) => {
+      const contactList = JSON.parse(data);
+      console.table(contactList);
+    })
+    .catch((err) => console.log(err.message));
+}
+
+function getContactById(contactId) {
+  // ...tu código
+
+  fs.readFile(contactPath)
+    .then((data) => {
+      const contacts = JSON.parse(data);
+      const contactExist = contacts.find((contact) => contact.id === contactId);
+      if (contactExist) {
+        console.log(contactExist);
+      } else {
+        console.log(`this contact does not exist`);
+      }
+    })
+    .catch((err) => console.log(err.message));
+}
+
+function removeContact(contactId) {
+  // ...tu código
+  fs.readFile(contactPath)
+    .then((data) => {
+      const contacts = JSON.parse(data);
+      const filteredContacts = contacts.filter(
+        (contact) => contact.id !== contactId
+      );
+      // New contacts
+      fs.writeFile(contactPath, JSON.stringify(filteredContacts)).then(() =>
+        console.log(`Done`)
+      );
+    })
+    .catch((err) => console.log(err.message));
+}
+
+function addContact(name, email, phone) {
+  // ...tu código
+  const newContact = {
+    // id: nanoid(),
+    name,
+    email,
+    phone,
+  };
+
+  fs.readFile(contactPath)
+    .then((data) => {
+      const contactList = JSON.parse(data);
+      contactList.push(newContact);
+      console.log(contactList);
+    })
+    .catch((err) => console.log(err.message));
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
---
+++ index.js
@@ -0,0 +1,50 @@
+const contacts = require("./contacts.js");
+const { Command } = require("commander");
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+// TODO: refactorizar
+function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      // ...
+      contacts.listContacts();
+      break;
+
+    case "get":
+      // ... id
+      contacts.getContactById(id);
+      break;
+
+    case "add":
+      // ... name email phone
+      contacts.addContact(name, email, phone);
+      break;
+
+    case "remove":
+      // ... id
+      contacts.removeContact(id);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(argv);
+
+// console.log(
+//   contacts.listContacts()
+//   // contacts.getContactById("AeHIrLTr6JkxGE6SN-0Rw")
+//   // contacts.removeContact("qdggE76Jtbfd9eWJHrssH")
+//   // contacts.addContact("Jorge", "jorge.com", "55555")
+// );
